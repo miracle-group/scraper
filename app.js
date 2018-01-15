d5759e74089873311fa699b81af732f47d0fe08b
Rizky Agung Saputro <co.terbang@gmail.com>
2018-01-15 20:36:34 +0700

    scrape server is only managing cron

--- app.js
+++ app.js
@@ -7,25 +7,26 @@ const app               = require('express')()
       combine           = require('./helpers/combined')
       mongoose          = require('mongoose')
       articleController = require('./controllers/articleController')
-
+      axios             = require('axios')
+      // scrap             = require('./gcf')
 
 app.use(logger('dev'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-mongoose.connection.openUri('mongodb://hary:hary@cluster0-shard-00-00-dvvn1.mongodb.net:27017,cluster0-shard-00-01-dvvn1.mongodb.net:27017,cluster0-shard-00-02-dvvn1.mongodb.net:27017/repod?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err,db) => {
-  if (err) {
-    console.log('TIDAK TERHUBUNG KE DATABASE')
-  } else {
-    console.log('DATABASE TERHUBUNG!')
-  }
-})
+// mongoose.connection.openUri('mongodb://hary:hary@cluster0-shard-00-00-dvvn1.mongodb.net:27017,cluster0-shard-00-01-dvvn1.mongodb.net:27017,cluster0-shard-00-02-dvvn1.mongodb.net:27017/repod?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin', (err,db) => {
+//   if (err) {
+//     console.log('TIDAK TERHUBUNG KE DATABASE')
+//   } else {
+//     console.log('DATABASE TERHUBUNG!')
+//   }
+// })
 
 let categories = []
 
 //scrape the categories
 
-const job = new CronJob ('0 55 * * * *', () => {
+const job = new CronJob (' 4 * * * *', () => {
   categories = []
   category.getCategory (datas => {
     datas.forEach(data => {
@@ -38,7 +39,11 @@ const job = new CronJob ('0 55 * * * *', () => {
 
 const scrapeJob = new CronJob ('10 * * * * *', () => {
   console.log('sekarang lagi ngescrape ', categories[0])
-  combine.getCombined(categories[0], articleController.addArticle)
+  // combine.getCombined(categories[0], articleController.addArticle)
+  axios.post('http://localhost:8010/project-sampingan/us-central1/index', {
+    topic: categories[0]
+  }).then(suc => console.log(suc))
+  .catch(err => console.log(err))
   categories = categories.slice(1)
   console.log(`categories sekarang adalah ${categories}`)
 
